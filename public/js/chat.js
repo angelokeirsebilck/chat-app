704531ebb0b9cd62ad15aa3a3a0573ea804d3179
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2020-12-10 07:48:58 +0100

    Test vh fix

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,18 @@
 const socket = io()
 
+// Fix 100vh problem mobile
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 // Elements
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
